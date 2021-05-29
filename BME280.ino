fafb484e2c7e682ad4c7f2c046d182801db65294
ME-DataStudio <75776348+ME-DataStudio@users.noreply.github.com>
2021-05-29 08:47:15 +0200

    Update BME280.ino
    Added MQTT

--- BME280.ino
+++ BME280.ino
@@ -7,13 +7,14 @@
   Uses Farmer Keith BMP280/BME280 library:
     https://github.com/farmerkeith/BMP280-library
   
-  28-05-2021: Addes wifi.
-
+  28-05-2021: Added wifi.
+  29-05-2021: Added MQTT. 
 ************************************************************************/
 #include <M5StickC.h>
 #include <farmerkeith_BMP280.h>
 #include <Wire.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 //BME sensor
 BME280 bme;
@@ -21,12 +22,16 @@ BME280 bme;
 //Wifi constants and variables
 const char* ssid     = "your_network_name";
 const char* password = "your_network_password";
+const int httpPort = 80;
 IPAddress local_IP(192, 168, 1, 210);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 0, 0);
 WiFiClient client;
-const int httpPort = 80;
-const int mqttPort = 1880;
+
+//MQTT
+const char* mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
+const int mqttPort = 1883;
+PubSubClient MQTTclient(client);
 
 void setup() 
 {
@@ -61,12 +66,10 @@ void setup()
   Serial.println(WiFi.subnetMask());
   Serial.print("Gateway IP: ");
   Serial.println(WiFi.gatewayIP());
-  // Use WiFiClient class to create TCP connections
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
   
+  // Use WiFiClient class to create connection to MQTT broker
+  MQTTclient.setServer(mqtt_server, mqttPort);
+  //MQTTclient.setCallback(callback);
   
   //I2C ans BME sensor
   Wire.begin();
@@ -78,30 +81,72 @@ void setup()
 
 int textColor=YELLOW;
 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!MQTTclient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (MQTTclient.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      MQTTclient.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(MQTTclient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+//MAIN loop
 void loop() 
 {
-  //BME variables declaration and assignment
-  double temp;
-  double druk;
-  double vocht=bme.readHumidity(temp,druk);
+  if (!MQTTclient.connected()) {
+    reconnect();
+  }
+  MQTTclient.loop();
   
-  //TFT print
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print("Temp=");
-  M5.Lcd.println(temp,2);
-  M5.Lcd.print("Vocht=");
-  M5.Lcd.println(vocht,2);
-  M5.Lcd.print("Druk=");
-  M5.Lcd.println(druk,2);
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+  
+    //BME variables declaration, assignment and conversion
+    double temp;
+    double druk;
+    double vocht=bme.readHumidity(temp,druk);
+    char tempString[8];
+    char drukString[8];
+    char vochtString[8];
+    dtostrf(temp, 1, 2, tempString);
+    dtostrff(druk, 1, 2, drukString);
+    dtostrf(vocht, 1, 2, vochtString);
+  
+    //TFT print
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print("Temp=");
+    M5.Lcd.println(temp,2);
+    M5.Lcd.print("Vocht=");
+    M5.Lcd.println(vocht,2);
+    M5.Lcd.print("Druk=");
+    M5.Lcd.println(druk,2);
 
-  //Serial monitor
-  Serial.printf("Temperatuur: %2.2f*C\n\r"  , temp);
-  Serial.printf("Vocht: %2.2f\%\n\r", vocht);
-  Serial.printf("Druk: %2.2fhPa\n\r", druk);
+    //Serial monitor
+    Serial.print("Temperatuur: ");
+    Serial.println(tempString);
+    Serial.print("Luchtdruk: ");
+    Serial.println(drukString);
+    Serial.print("Vocht: ");
+    Serial.println(vochtString);
   
-  delay(1000);
-  
-  if(textColor==YELLOW) textColor=GREEN;
-  else textColor=YELLOW;
-  M5.Lcd.setTextColor(textColor,BLACK);  
+    // Send over with MQTT  
+    MQTTclient.publish("esp32/temperatuur", tempString);
+    MQTTclient.publish("esp32/druk", drukString);
+    MQTTclient.publish("esp32/vocht", vochtString);
+    
+    if(textColor==YELLOW) textColor=GREEN;
+    else textColor=YELLOW;
+    M5.Lcd.setTextColor(textColor,BLACK);  
+  }
 }
